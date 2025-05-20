6f6395e016ac3a94dac83380d8bdd3a1fa96757b
patrickhuang <huang_zhe_min@163.com>
2025-05-20 16:50:27 +0800

    feat: 添加合并有序数组的 Solidity 合约
    新增 `MergeSortedArray` 合约，实现合并两个有序 uint 数组并返回新有序数组的功能。该功能适用于需要合并排序数组的场景。

---
+++ solidity-learning/task1/task1.5.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MergeSortedArray {
+    // 合并两个有序 uint 数组，返回一个新的有序数组
+    function merge(uint[] memory arr1, uint[] memory arr2) public pure returns (uint[] memory) {
+        uint len1 = arr1.length;
+        uint len2 = arr2.length;
+        uint[] memory result = new uint[](len1 + len2);
+        uint i = 0;
+        uint j = 0;
+        uint k = 0;
+        while (i < len1 && j < len2) {
+            if (arr1[i] <= arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+        while (i < len1) {
+            result[k++] = arr1[i++];
+        }
+        while (j < len2) {
+            result[k++] = arr2[j++];
+        }
+        return result;
+    }
+}
